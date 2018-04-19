1d0897516fd8b2d8e87807cf03ba346c686a32d5
bojanKalanj <bojan.kalanj@gmail.com>
2018-04-19 19:29:10 +0200

    index action for portfolio items

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   resources :portfolios
   get 'pages/home'
 
-  get 'pages/aboutcontact'
+  get 'pages/about'
+
+  get 'pages/contact'
 
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
