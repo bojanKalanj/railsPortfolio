4de67789855c16e627bbd8d65ba63434d9846ef5
bojanKalanj <bojan.kalanj@gmail.com>
2018-04-22 13:14:28 +0200

    Implemented concerns for managing image data

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -3,6 +3,10 @@ class PortfoliosController < ApplicationController
     @portfolio_items = Portfolio.all
   end
 
+  def angular
+    @angular_portfolio_items = Portfolio.angular
+  end
+
   def new
     @portfolio_item = Portfolio.new
   end
@@ -10,15 +14,11 @@ class PortfoliosController < ApplicationController
   def create
     @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
 
-    respond_to do |format|
       if @portfolio_item.save
-        format.html { redirect_to portfolios_path, notice: '@portfolio_item was successfully created.' }
-        format.json { render :show, status: :created, location: @portfolio_item }
+        redirect_to portfolios_path
       else
-        format.html { render :new }
-        format.json { render json: @portfolio_item.errors, status: :unprocessable_entity }
+        render 'new'
       end
-    end
   end
 
   def edit
--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,5 +1,19 @@
 class Portfolio < ApplicationRecord
-
+  include Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
 
+  def self.angular
+    where(subtitle: "Angular")
+  end
+
+  after_initialize :set_default
+
+  def set_default
+    # self.main_image ||= "http://via.placeholder.com/600x400"
+    self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
+    # self.thumb_image ||= "http://via.placeholder.com/350x200"
+    self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
+  end
+
+  # scope :ruby_on_rails_portfolio_items, -> { where(subtitle: "Angular") }
 end
